5a447a2caddc8ce65b3702f7eb53c74ce202c1ef
becar-dev <crezy007rp@gmail.com>
2025-07-26 01:19:51 +0500

    Configure PostgreSQL for production

--- olcha/settings.py
+++ olcha/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import dj_database_url
 from pathlib import Path
 import os
 
@@ -124,10 +124,11 @@ INTERNAL_IPS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # Agar DATABASE_URL muhit o'zgaruvchisi topilmasa, SQLite ishlatiladi
+        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
+        conn_max_age=600
+    )
 }
 
 
